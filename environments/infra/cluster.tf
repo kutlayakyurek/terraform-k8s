e1bc85517e01758a5faf50e95b7f778cc5837750
Kutlay Akyurek <kutlayakyurek@gmail.com>
2023-11-04 12:39:23 +0300

    change lb ip range

--- environments/infra/cluster.tf
+++ environments/infra/cluster.tf
@@ -5,10 +5,10 @@ module "cluster" {
   rke2_version               = "v1.28.2+rke2r1"
   rke2_ha_mode               = "true"
   rke2_ha_mode_kubevip       = "true"
-  rke2_api_ip                = "172.21.0.1"
+  rke2_api_ip                = "172.22.0.1"
   rke2_download_kubeconf     = "true"
   rke2_additional_sans       = ["infra-arus.trupt.com.tr"]
   rke2_server_options        = ["cluster-cidr: '172.20.0.0/16'", "service-cidr: '172.21.0.0/16'"]
-  rke2_loadbalancer_ip_range = "cidr-global: 172.21.0.0/22"
+  rke2_loadbalancer_ip_range = "cidr-global: 172.22.0.0/22"
   rke2_disable               = ["rke2-ingress-nginx"]
 }
