50089e74037372bae7ab7d57cd4067ae2d2c3dbe
Kutlay Akyurek <kutlayakyurek@gmail.com>
2023-11-04 18:58:28 +0300

    change fqdn

--- environments/infra/cluster.tf
+++ environments/infra/cluster.tf
@@ -6,9 +6,9 @@ module "cluster" {
   rke2_ha_mode               = "true"
   rke2_ha_mode_kubevip       = "true"
   rke2_ha_mode_keepalived    = "false"
-  rke2_api_ip                = "infra-proxy-arus.trupt.com.tr" # TODO this is registry machine with dnsmasq.. need proper proxy or load balancer ip
+  rke2_api_ip                = "infra-arus.trupt.com.tr" # TODO this is registry machine with dnsmasq.. need proper load balancer mechanism
   rke2_download_kubeconf     = "true"
-  rke2_additional_sans       = ["infra-arus.trupt.com.tr", "infra-proxy-arus.trupt.com.tr"]
+  rke2_additional_sans       = ["infra-arus.trupt.com.tr"]
   rke2_server_options        = ["cluster-cidr: 10.190.0.0/16", "service-cidr: 10.191.0.0/16"]
   rke2_loadbalancer_ip_range = "cidr-global: 10.192.0.0/22"
   rke2_disable               = "rke2-ingress-nginx"
