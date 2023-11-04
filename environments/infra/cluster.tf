ed4269817f7670029cd50ef919fe8ee7cda2248d
Kutlay Akyurek <kutlayakyurek@gmail.com>
2023-11-04 11:29:20 +0300

    change api ip

--- environments/infra/cluster.tf
+++ environments/infra/cluster.tf
@@ -10,5 +10,5 @@ module "cluster" {
   rke2_download_kubeconf     = "true"
   rke2_additional_sans       = ["infra-arus.trupt.com.tr"]
   rke2_server_options        = ["cluster-cidr: '10.42.0.0/16'", "service-cidr: '10.43.0.0/16'"]
-  rke2_loadbalancer_ip_range = "10.41.1.1-10.41.1.255"
+  rke2_loadbalancer_ip_range = "cidr-global: 10.41.1.0/24"
 }
